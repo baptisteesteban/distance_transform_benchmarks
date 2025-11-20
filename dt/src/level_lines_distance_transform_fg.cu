e7762e91097d8b62558febe6cd99b2dbaa5d2307
Baptiste Esteban <baptiste.esteban@epita.fr>
2025-11-20 10:46:16 +0100

    WIP handle border lldt fg

--- dt/src/level_lines_distance_transform_fg.cu
+++ dt/src/level_lines_distance_transform_fg.cu
@@ -18,19 +18,32 @@ namespace dt
   __global__ void pass(const std::uint8_t* m, const std::uint8_t* M, std::uint8_t* F, std::uint32_t* D, int width,
                        int height, int in_pitch, int out_pitch, bool* changed)
   {
-    const int t = threadIdx.x;
-    const int b = blockIdx.x;
-    const int y = b * blockDim.x + t;
-
+    // Order of traversal metadata
     const int     start_x = Forward ? 1 : width - 2;
     const int     end_x   = Forward ? width - 1 : 0;
     constexpr int inc     = Forward ? 1 : -1;
     constexpr int dx      = -1 * inc;
 
+    // Current line
+    const int      y      = blockIdx.x * blockDim.x + threadIdx.x;
     std::uint32_t* D_line = (std::uint32_t*)((std::uint8_t*)D + y * out_pitch);
 
-    if (y < height)
+    if (y == 0 || y == height - 1)
     {
+      for (int x = 0; x < width; x += 1)
+      {
+        D_line[x]           = 0;
+        F[y * in_pitch + x] = m[y * in_pitch + x];
+      }
+    }
+
+    if (y > 0 && y < height - 1)
+    {
+      D_line[0]                   = 0;
+      D_line[width - 1]           = 0;
+      F[y * in_pitch]             = m[y * in_pitch];
+      F[y * in_pitch + width - 1] = m[y * in_pitch + width - 1];
+
       for (int x = start_x; x != end_x; x += inc)
       {
         auto d     = D_line[x];
@@ -44,6 +57,13 @@ namespace dt
         }
       }
     }
+  }
+
+  // Top -> Bottom pass
+  template <bool Forward>
+  __global__ void pass_T(const std::uint8_t* m, const std::uint8_t* M, std::uint8_t* F, std::uint32_t* D, int width,
+                         int height, int in_pitch, int out_pitch, bool* changed)
+  {
   }
 
   void level_lines_distance_transform_fg_gpu(const image2d_view<std::uint8_t>& m, const image2d_view<std::uint8_t>& M,
