1452a227597ff092bfb6db77f358e6f998276788
Baptiste Esteban <baptiste.esteban@epita.fr>
2025-11-25 11:36:52 +0100

    WIP chessboard shared memory

--- dt/src/level_lines_distance_transform_chessboard.cu
+++ dt/src/level_lines_distance_transform_chessboard.cu
@@ -11,33 +11,29 @@
 namespace dt
 {
   static constexpr int BLOCK_SIZE = 16;
+  static constexpr int TILE_SIZE  = BLOCK_SIZE + 2; // Halo of 2 to handle border values
 
   // Top -> Bottom
   template <bool Forward>
-  __device__ bool pass_T(const image2d_view<std::uint8_t>& m, const image2d_view<std::uint8_t>& M,
-                         image2d_view<std::uint8_t>& F, image2d_view<std::uint32_t>& D)
+  __device__ bool pass_T(const std::uint8_t m[][TILE_SIZE], const std::uint8_t M[][TILE_SIZE],
+                         std::uint8_t F[][TILE_SIZE], std::uint32_t D[][TILE_SIZE])
   {
-    const int bx = blockIdx.x;
-    const int by = blockIdx.y;
-    const int x  = bx * BLOCK_SIZE + threadIdx.x;
-    if (x == 0 || x >= m.width() - 1)
-      return false;
-
     constexpr int inc     = Forward ? 1 : -1;
     constexpr int dy      = -1 * inc;
-    const int     start_y = Forward ? by * BLOCK_SIZE + (by == 0) : std::min((by + 1) * BLOCK_SIZE - 1, m.height() - 2);
-    const int     end_y = Forward ? std::min((by + 1) * BLOCK_SIZE, m.height() - 1) : std::max(by * BLOCK_SIZE - 1, 0);
+    const int     start_y = Forward ? 1 : TILE_SIZE - 2;
+    const int     end_y   = Forward ? TILE_SIZE - 1 : 0;
+    const int     x       = threadIdx.x + 1;
 
     bool line_changed = false;
 
     for (int y = start_y; y != end_y; y += inc)
     {
-      const std::uint8_t  q     = clamp(F(x, y + dy), m(x, y), M(x, y));
-      const std::uint32_t new_d = D(x, y + dy) + minus_abs<std::uint32_t>(F(x, y + dy), q);
-      if (new_d < D(x, y))
+      const std::uint8_t  q     = clamp(F[y + dy][x], m[y][x], M[y][x]);
+      const std::uint32_t new_d = D[y + dy][x] + minus_abs<std::uint32_t>(F[y + dy][x], q);
+      if (new_d < D[y][x])
       {
-        F(x, y)      = q;
-        D(x, y)      = new_d;
+        F[y][x]      = q;
+        D[y][x]      = new_d;
         line_changed = true;
       }
     }
@@ -47,30 +43,25 @@ namespace dt
 
   // Left -> Right
   template <bool Forward>
-  __device__ bool pass(const image2d_view<std::uint8_t>& m, const image2d_view<std::uint8_t>& M,
-                       image2d_view<std::uint8_t>& F, image2d_view<std::uint32_t>& D)
+  __device__ bool pass(const std::uint8_t m[][TILE_SIZE], const std::uint8_t M[][TILE_SIZE],
+                       std::uint8_t F[][TILE_SIZE], std::uint32_t D[][TILE_SIZE])
   {
-    const int bx = blockIdx.x;
-    const int by = blockIdx.y;
-    const int y  = by * BLOCK_SIZE + threadIdx.x;
-    if (y == 0 || y >= m.height() - 1)
-      return false;
-
     constexpr int inc     = Forward ? 1 : -1;
     constexpr int dx      = -1 * inc;
-    const int     start_x = Forward ? std::max(1, bx * BLOCK_SIZE) : std::min((bx + 1) * BLOCK_SIZE - 1, m.width() - 2);
-    const int     end_x   = Forward ? std::min((bx + 1) * BLOCK_SIZE, m.width() - 1) : std::max(bx * BLOCK_SIZE - 1, 0);
+    const int     start_x = Forward ? 1 : TILE_SIZE - 2;
+    const int     end_x   = Forward ? TILE_SIZE - 1 : 0;
+    const int     y       = threadIdx.x + 1;
 
     bool line_changed = false;
 
     for (int x = start_x; x != end_x; x += inc)
     {
-      const std::uint8_t  q     = clamp(F(x + dx, y), m(x, y), M(x, y));
-      const std::uint32_t new_d = D(x + dx, y) + minus_abs<std::uint32_t>(F(x + dx, y), q);
-      if (new_d < D(x, y))
+      const std::uint8_t  q     = clamp(F[y][x + dx], m[y][x], M[y][x]);
+      const std::uint32_t new_d = D[y][x + dx] + minus_abs<std::uint32_t>(F[y][x + dx], q);
+      if (new_d < D[y][x])
       {
-        F(x, y)      = q;
-        D(x, y)      = new_d;
+        F[y][x]      = q;
+        D[y][x]      = new_d;
         line_changed = true;
       }
     }
@@ -82,16 +73,51 @@ namespace dt
                                     image2d_view<std::uint8_t>& F, image2d_view<std::uint32_t>& D, bool even,
                                     bool* changed)
   {
-    if (!even && blockIdx.x % 2 == blockIdx.y % 2)
-      return;
-    if (even && blockIdx.x % 2 != blockIdx.y % 2)
+    const int bx    = blockIdx.x;
+    const int by    = blockIdx.y;
+    const int cur_x = bx * BLOCK_SIZE;
+    const int cur_y = by * BLOCK_SIZE + threadIdx.x;
+
+    if (cur_y >= m.height())
       return;
 
+    if (!even && bx % 2 == by % 2)
+      return;
+    if (even && bx % 2 != by % 2)
+      return;
+
+    // Loading tile into shared memory
+    __shared__ std::uint8_t s_m[TILE_SIZE][TILE_SIZE];
+    __shared__ std::uint8_t s_M[TILE_SIZE][TILE_SIZE];
+    __shared__ std::uint8_t s_F[TILE_SIZE][TILE_SIZE];
+    __shared__ std::uint32_t s_D[TILE_SIZE][TILE_SIZE];
+
+    {
+      const std::uint8_t m0 = m(0, 0);
+      for (int _dx = -1; _dx <= BLOCK_SIZE; _dx++)
+      {
+        const int dx = cur_x + _dx;
+        for (int _dy = -BLOCK_SIZE; _dy <= BLOCK_SIZE; _dy += BLOCK_SIZE)
+        {
+          const int oy = threadIdx.x + _dy;
+          if (oy < 0 || oy >= TILE_SIZE)
+            continue;
+
+          const int dy     = cur_y + _dy;
+          s_m[oy][_dx + 1] = dy >= 0 && dx >= 0 && dx < m.width() && dy < m.height() ? m(dx, dy) : m0;
+          s_M[oy][_dx + 1] = dy >= 0 && dx >= 0 && dx < m.width() && dy < m.height() ? M(dx, dy) : m0;
+          s_F[oy][_dx + 1] = dy >= 0 && dx >= 0 && dx < m.width() && dy < m.height() ? F(dx, dy) : m0;
+          s_D[oy][_dx + 1] = dy >= 0 && dx >= 0 && dx < m.width() && dy < m.height() ? D(dx, dy) : 0;
+        }
+      }
+    }
+    __syncthreads();
+
+    // Block level distance transform
     __shared__ int block_changed;
     if (threadIdx.x == 0)
       block_changed = 1;
     __syncthreads();
-
     while (block_changed)
     {
       if (threadIdx.x == 0)
@@ -99,13 +125,13 @@ namespace dt
       __syncthreads();
 
       int t_changed = 0;
-      t_changed += pass<true>(m, M, F, D);
+      t_changed += pass<true>(s_m, s_M, s_F, s_D);
       __syncthreads();
-      t_changed += pass<false>(m, M, F, D);
+      t_changed += pass<false>(s_m, s_M, s_F, s_D);
       __syncthreads();
-      t_changed += pass_T<true>(m, M, F, D);
+      t_changed += pass_T<true>(s_m, s_M, s_F, s_D);
       __syncthreads();
-      t_changed += pass_T<false>(m, M, F, D);
+      t_changed += pass_T<false>(s_m, s_M, s_F, s_D);
       __syncthreads();
 
       if (t_changed)
@@ -116,6 +142,18 @@ namespace dt
         *changed = true;
       __syncthreads();
     }
+
+    // Loading tile into global memory
+    for (int _dx = 1; _dx <= BLOCK_SIZE; _dx++)
+    {
+      const int dx = cur_x + _dx - 1;
+      if (dx < m.width())
+      {
+        D(dx, cur_y) = s_D[threadIdx.x + 1][_dx];
+        F(dx, cur_y) = s_F[threadIdx.x + 1][_dx];
+      }
+    }
+    __syncthreads();
   }
 
   void level_lines_distance_transform_chessboard_gpu(const image2d_view<std::uint8_t>& m,
@@ -141,13 +179,15 @@ namespace dt
 
     dim3 grid_dim(m.width() / BLOCK_SIZE + 1, m.height() / BLOCK_SIZE + 1);
     dim3 block_dim(BLOCK_SIZE);
-    bool even = true;
-    while (*changed)
+    bool even    = false;
+    int  nrounds = 0;
+    while (*changed && nrounds < 1)
     {
       *changed = false;
       block_propagation<<<grid_dim, block_dim>>>(m, M, F, D, even, changed);
       cudaDeviceSynchronize();
       even = !even;
+      nrounds += 1;
     }
     if (const auto err = cudaGetLastError(); err != cudaSuccess)
       throw std::runtime_error(
