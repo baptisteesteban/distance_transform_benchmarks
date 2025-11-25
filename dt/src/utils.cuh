bad975cbcb121ce87abe81cb24cb9dd8be061e49
Baptiste Esteban <baptiste.esteban@epita.fr>
2025-11-25 16:23:43 +0100

    Specify return type for clamp

--- dt/src/utils.cuh
+++ dt/src/utils.cuh
@@ -9,7 +9,7 @@ namespace dt
   __global__ void init(const image2d_view<std::uint8_t>& m, image2d_view<std::uint32_t>& D,
                        image2d_view<std::uint8_t>& F);
 
-  __inline__ __device__ int clamp(std::uint8_t v, std::uint8_t m, std::uint8_t M)
+  __inline__ __device__ std::uint8_t clamp(std::uint8_t v, std::uint8_t m, std::uint8_t M)
   {
     return v < m ? m : (v > M ? M : v);
   }
