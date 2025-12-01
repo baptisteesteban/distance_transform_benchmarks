7f19c1c0a70dfe078698c0b0e70a743350a605f4
Baptiste Esteban <baptiste.esteban@epita.fr>
2025-12-01 16:52:50 +0100

    Start testing euclidian distance transform

---
+++ bin/euclidian_distance_transform.cpp
@@ -0,0 +1,26 @@
+#include <dt/fill.hpp>
+#include <dt/geodesic_distance_transform.hpp>
+#include <dt/imsave.hpp>
+#include <dt/inferno.hpp>
+#include <dt/normalize.hpp>
+#include <dt/transfert.hpp>
+
+int main(int argc, char* argv[])
+{
+  auto img  = dt::image2d<std::uint8_t>(50, 40);
+  auto mask = dt::image2d<std::uint8_t>(50, 40);
+  dt::fill(img, std::uint8_t(0));
+  dt::fill(mask, std::uint8_t(1));
+  for (int y = 20; y < 30; y++)
+  {
+    for (int x = 20; x < 43; x++)
+      mask(x, y) = 0;
+  }
+  const auto d_img   = dt::host_to_device(img);
+  const auto d_mask  = dt::host_to_device(mask);
+  const auto d_dist  = dt::geodesic_distance_transform(d_img, d_mask, 1e10, 0, 1, 100);
+  const auto dist    = dt::device_to_host(d_dist);
+  const auto norm    = dt::normalize<std::uint8_t>(mask);
+  const auto colored = dt::inferno(norm);
+  dt::imsave("out.png", colored);
+}
--- dt/include/dt/transfert.hpp
+++ dt/include/dt/transfert.hpp
@@ -11,17 +11,17 @@ namespace dt
   void device_to_host(const image2d_view<void>& src, image2d_view<void>& dst);
 
   template <typename T>
-  image2d<T> host_to_device(const image2d<T>& src);
+  image2d<T> host_to_device(const image2d_view<T>& src);
 
   template <typename T>
-  image2d<T> device_to_host(const image2d<T>& src);
+  image2d<T> device_to_host(const image2d_view<T>& src);
 
   /*
    * Implementations
    */
 
   template <typename T>
-  image2d<T> host_to_device(const image2d<T>& src)
+  image2d<T> host_to_device(const image2d_view<T>& src)
   {
     assert(src.memory_kind() == e_memory_kind::CPU);
     image2d<T> dst(src.width(), src.height(), e_memory_kind::GPU);
@@ -30,7 +30,7 @@ namespace dt
   }
 
   template <typename T>
-  image2d<T> device_to_host(const image2d<T>& src)
+  image2d<T> device_to_host(const image2d_view<T>& src)
   {
     assert(src.memory_kind() == e_memory_kind::GPU);
     image2d<T> dst(src.width(), src.height(), e_memory_kind::CPU);
